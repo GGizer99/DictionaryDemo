289303259e16b630b5d56f1dab55ca1d58c02da8
gizer dogan <gizerdogan@hotmail.com>
2021-01-23 02:22:36 +0300

    Add project files.

---
+++ DictionaryDemo/MyDictionary.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DictionaryDemo
+{
+    class MyDictionary<T1,T2>
+    {
+        T1[] _tempkeys;
+        T2[] _tempvalues;
+       
+        T1[] _keys;
+        T2[] _values;
+        
+        public MyDictionary()
+        {
+            _keys = new T1[0];
+            _values = new T2[0];
+        }
+
+        public void Add(T1 key,T2 value)
+        {
+            _tempkeys = _keys;
+            _tempvalues = _values;
+
+            _keys = new T1[_keys.Length + 1];
+            _values = new T2[_values.Length + 1];
+
+            for (int i = 0; i < _tempkeys.Length; i++)
+            {
+                _keys[i] = _tempkeys[i];
+                _values[i] = _tempvalues[i];
+            }
+
+            _keys[_keys.Length - 1] = key;
+            _values[_values.Length - 1] = value;
+
+
+
+        }
+
+       
+
+        public int Count
+        {
+            get { return _values.Length; }
+        }
+
+        public T1[] Keys
+        {
+            get { return _keys; }
+        }
+
+        public T2[] Value
+        {
+            get { return _values; }
+        }
+
+   
+
+
+    }
+}
---
+++ DictionaryDemo/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DictionaryDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> players = new MyDictionary<int, string>(); 
+
+            players.Add(35, "Cihan Topaloğlu");
+            players.Add(4, "Tolga Ünlü");
+            players.Add(53, "Lokman Gör");
+            players.Add(88, "Özgür Özkaya");
+            players.Add(38, "İbrahim Öztürk");
+            players.Add(8, "Yekta Kurtuluş");
+            players.Add(19, "Marco Paixao");
+
+            Console.WriteLine("-----------Takım----------");
+            for (int i = 0; i < players.Keys.Length; i++)
+            {
+                Console.WriteLine(+ players.Keys[i] + " - " +  players.Value[i]);
+            }
+
+
+            Console.WriteLine("-------------Oyuncu Sayısı------------");
+            Console.WriteLine(players.Count);
+
+           
+
+            /*foreach (var number in players.Keys )
+            {
+                Console.WriteLine(number);
+            }
+            foreach (var name in players.Value)
+            {
+                Console.WriteLine(name);
+            }*/
+
+        }
+    }
+}
